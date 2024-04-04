f4188d7219096c84e3c358bc4a44ad1825baeec6
Matthieu Peeters <m.peeters@iknl.nl>
2024-04-05 00:05:01 +0200

    Add SQLPage

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # wb
+
+
+
+## start: 
+
+docker compose -f docker-compose.yml up
+
+## connect:
+
+docker exec -it <mycontainer> bash
---
+++ sqlpage/index.sql
@@ -0,0 +1,4 @@
+SELECT 'list' AS component, 'Popular websites' AS title;
+
+SELECT 'Hello' AS title, 'world' AS description, 'https://wikipedia.org' AS link;
+
